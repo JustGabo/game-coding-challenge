1ee659bf908a315345c88f62ebf8944f143688fa
JustGabo <ovallegabriel123@gmail.com>
2024-12-07 19:30:41 -0400

    making improvenments in the collect button

--- app/game/[id]/components/collectButton/index.tsx
+++ app/game/[id]/components/collectButton/index.tsx
@@ -1,31 +1,49 @@
-"use client"
-import { Button } from '../../../../../components/ui/button'
-import useGamesStore from '@/stores/gamesStore'
-import { Game } from '@/app/types/game'
-import { toast } from '@/hooks/use-toast'
+"use client";
+import { Button } from "../../../../../components/ui/button";
+import useGamesStore from "@/stores/gamesStore";
+import { Game } from "@/app/types/game";
+import { toast } from "@/hooks/use-toast";
+import { useEffect, useState } from "react";
 
-interface CollectButtonProps{
-  game: Game
+interface CollectButtonProps {
+  game: Game;
 }
 
-const CollectButton = ({game}: CollectButtonProps) => {
+const CollectButton = ({ game }: CollectButtonProps) => {
+  const { addGame, isGameCollected } = useGamesStore();
+  const [isMounted, setIsMounted] = useState(false);
 
-  const {addGame, isGameCollected} = useGamesStore()
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-  const isCollected = isGameCollected(game.id)
+  const isCollected = isMounted ? isGameCollected(game.id) : false;
 
-  const collectGame = ()=>{
-    addGame(game)
+  const collectGame = () => {
+    addGame(game);
     toast({
       title: "Game added to collection",
       description: `${game.name} has been added to your collection`,
       variant: "success",
     });
-  }
+  };
+
+  if (!isMounted) return null;
 
   return (
-    <Button onClick={collectGame} style={{backgroundColor: isCollected ? "white" : "#3C1661", color: isCollected ? "#3C1661" : "white", border: isCollected ? "1px solid #3C1661" : "none"}} className={`w-full rounded-full h-[45px] `}>Collect game</Button>
-  )
-}
+    <Button
+      onClick={collectGame}
+      disabled={isCollected}
+      style={{
+        backgroundColor: isCollected ? "white" : "#3C1661",
+        color: isCollected ? "#3C1661" : "white",
+        border: isCollected ? "1px solid #3C1661" : "1px solid white",
+      }}
+      className={`w-full rounded-full h-[45px] `}
+    >
+      {isCollected ? "Collected" : "Collect game"}
+    </Button>
+  );
+};
 
-export default CollectButton
+export default CollectButton;
